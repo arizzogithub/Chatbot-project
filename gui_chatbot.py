c6558ee3fc3e77e5cf0d6342a1eb26c9df106d6b
Anastasia Rizzo <55031920+arizzogithub@users.noreply.github.com>
2022-07-03 23:47:18 +0200

    Update gui_chatbot.py

--- gui_chatbot.py
+++ gui_chatbot.py
@@ -59,7 +59,7 @@ def getResponse(ints, intents_json):
     return result
 
 
-#Creating tkinter GUI
+# Creating tkinter GUI
 import tkinter
 from tkinter import *
 
@@ -86,26 +86,26 @@ root.title("Chatbot")
 root.geometry("400x500")
 root.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
+# Create Chat window
 ChatBox = Text(root, bd=0, bg="white", height="8", width="50", font="Arial",)
 
 ChatBox.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
+# Bind scrollbar to Chat window
 scrollbar = Scrollbar(root, command=ChatBox.yview, cursor="heart")
 ChatBox['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
+# Create Button to send message
 SendButton = Button(root, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
                     bd=0, bg="#f9a602", activebackground="#3c9d9b",fg='#000000',
                     command= send )
 
-#Create the box to enter message
+# Create the box to enter message
 EntryBox = Text(root, bd=0, bg="white",width="29", height="5", font="Arial")
 #EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
+# Place all components on the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatBox.place(x=6,y=6, height=386, width=370)
 EntryBox.place(x=128, y=401, height=90, width=265)
